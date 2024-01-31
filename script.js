746e825cb08c4a69dc30cc661cfd44963348e9ce
Hana Pavlová <hanapavlova7@gmail.com>
2024-01-31 20:23:25 +0100

    ukol2 promenne

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
 let cislo = Math.floor(Math.random()*6)+1;
 
-document.body.innerHTML = cislo;
+document.body.innerHTML += cislo;
+
+let cena = 12;
+let pocetNavstevniku = 174;
+let pocetPredstaveni = 15;
+let prijem = cena*pocetNavstevniku*pocetPredstaveni
 
