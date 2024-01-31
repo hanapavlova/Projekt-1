ccde4460ccdbe56b9a34859477326f5a5ebe4cda
Hana Pavlová <hanapavlova7@gmail.com>
2024-01-31 20:46:12 +0100

    ukol2 komplet

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ let prijem = cena*pocetNavstevniku*pocetPredstaveni
 
 document.body.innerHTML += "<p>Mesicni prijem divadla ze vstupneho je " + prijem + " EUR. </p>";
 
+let cenaStudent = 12*0.65;
+let pocetStudentu = 174*0.4
+let pocetDospelych = 174*0.6
+
+let prijem2 = (cena*pocetDospelych + cenaStudent*pocetStudentu)*pocetPredstaveni
+
+document.body.innerHTML += "<p>Se studentskou slevou je mesicni prijem divadla " + prijem2 + " EUR.</p>";
+
